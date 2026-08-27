45f79888f4409f0d8771c7267a220d6f8bd5de2c
Emy <emily.lara270719@potros.itson.edu.mx>
2026-08-27 10:20:54 -0700

    Initial commit

---
+++ src/main/java/com/example/MiPrimerServidor/HelloControler.java
@@ -0,0 +1,23 @@
+package com.example.MiPrimerServidor;
+// En esta clase es la que va a recibir peticiones http
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api") // Le estamos indicando que solo lo hará cuando haya /api
+public class HelloControler {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hola desde mi Servidor de Aplicación";
+    }
+
+    @GetMapping("/saludo")
+    public String saludar(@RequestParam String nombre){
+        return "Hola: " +nombre+ " desde mi servidor de apps";
+    }
+
+    @PostMapping("/mensaje") // POST es para guardar
+    public String recibirMensaje(@RequestBody String mensaje){ // Se puede enviaar un objeto completo como parametro
+        return "recibí el mensaje "+mensaje;
+    }
+}
